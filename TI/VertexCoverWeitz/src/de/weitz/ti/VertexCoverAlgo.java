dfaff1637bcecefd5b513e356ff22d093b376619
JanO Mnemonix <jmnemonix@sl9.eu>
2015-05-27 18:23:04 +0200

    reset

--- TI/VertexCoverWeitz/src/de/weitz/ti/VertexCoverAlgo.java
+++ TI/VertexCoverWeitz/src/de/weitz/ti/VertexCoverAlgo.java
@@ -16,72 +16,16 @@ class VertexCoverAlgo {
 
   boolean[][] edges;
   int max;
-  
-  boolean ausgabe = true;
-  boolean[] gesetzt;
-  int[] hatKante;
 
   VertexCoverAlgo(boolean edges[][]) {
     this.edges = edges;
     this.max = edges.length - 1;
-
-    this.gesetzt  = new boolean[edges.length];
-    this.hatKante = new int[edges.length];
-    
   }
 
   // this is where you should write your code 
   int[] solve() {
     int solution[] = {4,3,5,6};
 //	  int solution[];
-    
-    if(ausgabe){
-	    // Ausgabe
-	    String s = "";
-	    for( int i=1; i <= max ; i++){
-	    	s = s+"\n"+i+" |";
-	    	for(int j=1; j <= max ; j++) s=s+(edges[i][j]  ? " 1" : " 0");
-	    }
-	    printer(s+"\n\n");
-    }
-    
-    // Suchen
-    boolean gefunden = false;
-//    while(!gefunden){
-//    	printer()
-//	    for( int i=1; i <= max ; i++){
-//	    	for(int j=1; j <= max ; j++){
-//	    		
-//	    	}
-//	    }
-//    }
-//    for(int k=0 ; k < Math.pow(2,max) ; k++){
-//		printer("====== Durchlauf :"+k+" ======");
-//		
-//	}
-    // Wer hat kanten??
-    for( int i=1; i <= max ; i++){
-    	boolean hat = false;
-    	for(boolean set : edges[i]) if(set) hatKante[i]++;
-    }
-    
-    for( int i=1 ; i <= max ; i++){
-    	// wenn keine kante gleich weiter
-    	if(hatKante[i] <= 0) continue;
-    	// sonst gucken
-    	for(int j=i+1; j <= max ; j++){
-    		
-    	}
-    }
-    for(int k=1 ; k < Math.pow(2,max) ; k++){
-    	printer("====== Durchlauf :"+k+" ======");
-    	printer(""+max);
-    	
-    }
-    
-    for( int i=1 ; i <= 8 ; i++){
-    	printer(Integer.toBinaryString(i));
-    }
     
     return solution;
   }
